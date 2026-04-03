7a56c7bf6e13b579e56b67e0a4bc455721fdc59b
Saika Islam <saikaislam0786@gmail.com>
2026-04-03 08:27:38 +0530

    Optimize prime number check logic
    Updated the prime number check to iterate only up to the square root of n for efficiency.

--- Java Basic Programming/JavaBasics/PrimeNum.java
+++ Java Basic Programming/JavaBasics/PrimeNum.java
@@ -7,7 +7,7 @@ class PrimeNum {
             System.out.println("n is prime");
         }else{
             boolean isPrime = true;
-            for(int i=2; i<n-1; i++){
+            for(int i=2; i<Math.sqrt(n); i++){
                 if(n%i == 0){
                     isPrime = false;
                 }
