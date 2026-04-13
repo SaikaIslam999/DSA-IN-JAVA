03a75d49e2838f250bf4277f822a6a7e001794e5
Saika Islam <saikaislam0786@gmail.com>
2026-04-13 10:38:54 +0530

    Update Subarrays.java

--- ARRAY/Subarrays.java
+++ ARRAY/Subarrays.java
@@ -1,18 +1,18 @@
 public class Subarrays {
     public static void printSubarray(int numbers[]){
-        int ts = 0;
+        int totSum = 0;
         for(int i=0; i<numbers.length; i++){
             for(int j=i; j<numbers.length; j++){
                 for(int k=i; k<=j; k++){
                     System.out.print(numbers[k]+ " ");
                 }
-                ts++;
+                totSum++;
                 System.out.println();
             }
             
             System.out.println();
         }
-        System.out.println("total sum : " + ts);
+        System.out.println("total sum : " + totSum);
     }
     public static void main(String args[]){
       int numbers[] = {2 , 4 , 6 , 8 , 10};
