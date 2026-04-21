eb0e530e32c768d3df8475def937cd67aeef884d
Saika Islam <saikaislam0786@gmail.com>
2026-04-21 08:59:00 +0530

    Implement staircase search method in SearchInSorted
    Added a new staircase search method for finding a key in a sorted matrix.

--- ARRAY/SearchInSorted.java
+++ ARRAY/SearchInSorted.java
@@ -3,7 +3,7 @@ import java.util.*;
 //Problem : Search for key in sorted matrix
 
 public class SearchInSorted {
-    public static boolean staircaseSearch(int matrix[][], int key) {
+    public static boolean staircaseSearch(int matrix[][], int key) {   //Time Complexity --> O(n+m)
         int n = matrix.length, m = matrix[0].length;
         int i = 0, j = n-1;
 
@@ -26,6 +26,26 @@ public class SearchInSorted {
         System.out.println("NOT Found");
         return false;
     }
+
+    // bottom to right 
+    public static boolean staircase(int matrix[][], int key){
+        int row = matrix.length - 1;
+        int col = 0;
+        while(row >= 0 && col <= matrix[0].length-1){
+            if(key == matrix[row][col]){
+                System.out.println("key is found at index: "+ "("+ row+","+col+")");
+                return true;
+            }
+            else if(key < matrix[row][col]){
+                row--;
+            }else{
+                col++;
+            }
+        }
+        System.out.println("key is not found");
+        return false;
+    }
+    
     public static void main(String args[]) {
         int matrix[][] = {{10, 20, 30, 40},
                         {15, 25, 35, 45},
