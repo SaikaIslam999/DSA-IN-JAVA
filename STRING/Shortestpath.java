59d7f7b5e530b0818a6e43cc394b915180284fe7
Saika Islam <saikaislam0786@gmail.com>
2026-04-23 11:54:57 +0530

    Fix formatting issues in Shortestpath.java

--- STRING/Shortestpath.java
+++ STRING/Shortestpath.java
@@ -3,15 +3,19 @@ public class Shortestpath {
         int x = 0, y = 0;
         for(int i=0; i<path.length(); i++){
             char dir = path.charAt(i);
+            //south
             if(dir == 'S'){
                 y--;
             }
+            //north
             else if(dir == 'N'){
                     y++;
                 }
+            //west
             else if(dir == 'W'){
                 x--;
             }
+            //east
             else{
                 x++;
             }
