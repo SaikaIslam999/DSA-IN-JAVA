d7dd458401ca9a3c0a39a4eabdecf48b9086c791
Saika Islam <saikaislam0786@gmail.com>
2026-04-24 08:07:53 +0530

    Implement substring printing functionality
    Add a method to print a substring from a given string.

--- STRING/subString.java
+++ STRING/subString.java
@@ -1,1 +1,14 @@
-
+class Main {
+    public static String printSubstring(String str, int si, int ei){
+        String substring = "";
+        for(int i=si; i<ei; i++){
+            substring += str.charAt(i);
+        }
+        return substring;
+    }
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        System.out.println(printSubstring(str, 0, 5));
+        System.out.println(str.substring(0,7));
+    }
+}
