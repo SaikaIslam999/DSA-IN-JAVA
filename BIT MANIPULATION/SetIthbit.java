223ebec45076325e9b76851853093fe2017474b4
Saika Islam <saikaislam0786@gmail.com>
2026-04-27 11:28:47 +0530

    Clarify left shift operation in SetIthBit method
    Added comment to clarify the use of left shift operation.

--- BIT MANIPULATION/SetIthbit.java
+++ BIT MANIPULATION/SetIthbit.java
@@ -1,6 +1,6 @@
 public class SetIthbit {
     public static int SetIthBit(int n , int i){
-        int bitMark = 1<<i;
+        int bitMark = 1<<i;           //---> using leftashift
         return n|bitMark;
     }
   public static void main(String args[]){
