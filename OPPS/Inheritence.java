81d968c1209ae14446ce086e7ef4f9015f514663
Saika Islam <saikaislam0786@gmail.com>
2026-05-01 08:15:44 +0530

    Implement hybrid inheritance with Animal classes

--- OPPS/Inheritence.java
+++ OPPS/Inheritence.java
@@ -35,6 +35,61 @@ class Fish extends Animal{
     }
 }
 
+// Hybrid Inheritence
+
+public class Inheritence {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.breath();
+        d.talk();
+        Shark s = new Shark();
+        s.swim();
+        s.fins = 2;
+        System.out.println(s.fins);
+        Sparrow spa = new Sparrow();
+        spa.fly();
+        spa.eat();
+    }
+}
+
+class Animal{
+    void eat(){
+        System.out.println("eats...");
+    }
+    void breath(){
+        System.out.println("breath...");
+    }
+}
+class Mammal extends Animal{
+    int legs;
+}
+class Cat extends Mammal{
+    void talk(){
+        System.out.println("meow..");
+    }
+}
+class Dog extends Mammal{
+    void talk(){
+        System.out.println("bark..");
+    }
+}
+class Fish extends Animal{
+    void swim(){
+        System.out.println("swims");
+    }
+}
+class Shark extends Fish{
+    int fins;
+}
+class Bird extends Animal{
+    void fly(){
+        System.out.println("fly...");
+    }
+}
+class Sparrow extends Bird{
+    int legs;
+}
 
 
 
