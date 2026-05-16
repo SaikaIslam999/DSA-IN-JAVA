b23a4922b4501c0cf3f1bf7f50c35a03886ef514
Saika Islam <saikaislam0786@gmail.com>
2026-05-16 08:52:50 +0530

    Refactor recursion methods with commented alternatives
    Commented out alternative return statements for clarity.

--- RECURSION/RecursionBasics.java
+++ RECURSION/RecursionBasics.java
@@ -104,6 +104,8 @@ public class RecursionBasics {
 
         int totWays = fnm1 + fnm2;
         return totWays;
+
+        // return tilingProblem(n - 1) + tilingProblem(n - 2)  ---> short ways
     }
     public static void removeDuplicates(String str, int idx, StringBuilder newStr , boolean map[]){
         if(idx == str.length()){
@@ -132,7 +134,7 @@ public class RecursionBasics {
         // total ways
         int totWays = fnm1 + pairWays;
         return totWays;
-    //    return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
+    //    return friendsPairing(n-1) + (n-1) * friendsPairing(n-2); --> short ways
     }
     public static void printBinStr(int n,int lastplace,String str){
      // base case
