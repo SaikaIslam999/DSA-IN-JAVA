970a04299fbd3533b231e5bdfc522de32317283b
Saika Islam <saikaislam0786@gmail.com>
2026-06-21 20:30:20 +0530

    Update queueJCF.java

--- QUEUE/queueJCF.java
+++ QUEUE/queueJCF.java
@@ -1,7 +1,8 @@
 import java.util.*;
 class Main {
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
+        //Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
